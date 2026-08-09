7b8c8ea65936313c3ff77a5ff7302d40a8491ad8
luismiguel29 <rojashuallcoluismiguel29@gmail.com>
2026-08-08 20:33:02 -0400

    regresar tiempo normal botones aniumacion

--- public/cssGeneral/stud_AnadirMaterias.js
+++ public/cssGeneral/stud_AnadirMaterias.js
@@ -37,7 +37,7 @@ $("#idFrmAnadir").submit(function (e) {
       $("#idBtnAnadir").hide(0);
       setTimeout(function () {
          form.submit();
-      }, 6000);
+      }, 1000);
    //}
    //else {
    //   return false
--- public/cssGeneral/stud_ModificarMaterias.js
+++ public/cssGeneral/stud_ModificarMaterias.js
@@ -43,7 +43,7 @@ $("#idFrmModificar").submit(function (e) {
       $("#idBtnModificar").hide(0);
       setTimeout(function () {
          form.submit();
-      }, 7000);
+      }, 2000);
    }
    else {
       return false
